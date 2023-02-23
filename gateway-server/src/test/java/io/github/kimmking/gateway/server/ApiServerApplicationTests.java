76138c1af19fb1f41cf4a98d4bc582204d332375
kimmking <kimmking@163.com>
2023-02-23 15:24:55 +0800

    update springboot 2.7.6 test

--- gateway-server/src/test/java/io/github/kimmking/gateway/server/ApiServerApplicationTests.java
+++ gateway-server/src/test/java/io/github/kimmking/gateway/server/ApiServerApplicationTests.java
@@ -1,6 +1,6 @@
 package io.github.kimmking.gateway.server;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
